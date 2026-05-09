8ff8b7ac0913f18d439ecdee06960341db11991b
Lucas Hünniger <lucas.huenniger02@gmail.com>
2026-05-09 11:02:46 +0200

    chore: make blog more visual appealing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	base: "/",
-	site: "https://blog.lucas-huenniger.dev/",
-	integrations: [
-		starlight({
-			title: 'The Runtime',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/MhouneyLH/the_runtime_blog' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  base: "/",
+  site: "https://blog.lucas-huenniger.dev/",
+  integrations: [
+    starlight({
+      title: "The Runtime",
+      customCss: ["./src/styles/custom.css"],
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/MhouneyLH/the_runtime_blog" },
+      ],
+      sidebar: [
+        {
+          label: "Blog Posts",
+          items: [{ label: "Example Blog Post", slug: "blog_posts/example" }],
+        },
+      ],
+    }),
+  ],
 });
--- src/content/docs/blog_posts/example copy.md
+++ src/content/docs/blog_posts/example copy.md
@@ -1,6 +1,7 @@
 ---
-title: Example Guide
+title: Example Blog Post2
 description: A guide in my new Starlight docs site.
+pinned: true
 ---
 
 Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
